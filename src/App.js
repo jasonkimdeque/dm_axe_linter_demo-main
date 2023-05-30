ecf7f2496de11ab77a1c50e6f0bbec701da8b17c
jasonkimdeque <jason.kim@deque.com>
2023-05-30 09:02:10 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo"/> 
           <button id="empty"></button> 
-          <img src={logo} className="App-logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
